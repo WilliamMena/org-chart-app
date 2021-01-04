f824c9baf9a815060f3e973958aab310cb00f604
WilliamMena <williammena1991@gmail.com>
2021-01-04 01:53:48 -0500

    Add update method which updates parent_id of current node and it's children, (children optional)

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,4 +1,6 @@
 class NodesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     all_nodes = Node.all
     respond_to do |format|
@@ -6,4 +8,36 @@ class NodesController < ApplicationController
       format.json { render json: all_nodes }
     end
   end
+
+  def show
+    render json: Node.find(params[:id]) 
+  end
+
+  def update
+    @node = Node.find(params[:id])
+
+    # byebug
+    if node_param["bring_team"]
+      @node.parent_id = node_param["parent_id"]
+    else
+      parent = @node.parent_id
+      
+      Node.where(parent_id: @node.id).update_all(parent_id: parent)
+      @node.parent_id = node_param["parent_id"]
+    end
+
+    if @node.save
+      render json: { message: "Node successfully updated." }, status: 200
+    else
+      render json: { error: @node.errors.full_messages}, status: 400
+    end
+    
+    
+ end
+ 
+ private
+ def node_param
+    params.require(:node).permit(:parent_id, :bring_team)
+ end
+
 end
